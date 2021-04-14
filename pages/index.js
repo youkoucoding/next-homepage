dc35523148b094288b2d09a9d0b1f6c7f6419e4d
youkoucoding <youkousai@gmail.com>
2021-04-14 22:20:40 +0900

    initialize with nextjs&tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,38 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className='bg-gray-300'>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className=''>
+        <h1 className=''>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
+        <p className=''>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className=''>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div className='grid'>
+          <a href="https://nextjs.org/docs" className='flex'>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn" className='flex'>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            className=''
           >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
@@ -61,5 +61,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
