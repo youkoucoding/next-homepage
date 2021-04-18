1aaccb43edf7f2d8b5e4c38baad1643ff5eec642
youkoucoding <youkousai@gmail.com>
2021-04-18 18:19:44 +0900

    Add logo infinite scrolling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layout';
 import Header from '../components/header';
 import ListSection from '../components/listSection';
+import ScrollingLogo from '../components/scrollingLogo';
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <Layout>
       <main>
         <Header />
+        <ScrollingLogo />
         <ListSection />
       </main>
     </Layout>
