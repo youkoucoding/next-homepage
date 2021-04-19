1106cce969bae4db74af72fa71b450e8c4df36c8
youkoucoding <youkousai@gmail.com>
2021-04-19 23:00:39 +0900

    debug

--- components/header.js
+++ components/header.js
@@ -3,13 +3,13 @@ import Button from './button';
 const Header = () => (
   <header className='bg-gradient-to-t from-secondary to-opacity'>
     <div className='min-h-full'>
-      <div className='max-w-4xl mx-auto py-16 px-14 sm:px-6 lg:px-8'>
+      <div className='max-w-4xl mx-auto py-8 px-14 sm:px-6 lg:px-8'>
         <h1 className='font-sans font-bold text-4xl md:text-5xl lg:text-8xl text-center leading-snug text-gray-800'>
           Your website, beyond expectations
                 </h1>
       </div>
       <div className='max-w-xl mx-auto'>
-        <p className='mt-10 text-gray-500 text-center text-xl lg:text-3xl'>
+        <p className='mt-0 text-gray-500 text-center text-xl lg:text-3xl'>
           Make your website wonderful and build beyond your expectations.
                 </p>
       </div>
